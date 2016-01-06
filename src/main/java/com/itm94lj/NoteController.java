a011b2d448da0e7efa8d7133c848f9f8ae60cf2c
itm94lj <itm94lj@163.com>
2016-01-06 23:11:51 +0800

    jsp add

--- src/main/java/com/itm94lj/NoteController.java
+++ src/main/java/com/itm94lj/NoteController.java
@@ -4,13 +4,59 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class NoteController  {
+    private String value;
+    private String key;
+
+    @RequestMapping(value = "search", method = RequestMethod.GET)
+    public void searchNote(HttpServletRequest httpRequest, 
+              HttpServletResponse httpResponse) throws java.io.IOException
+ {
+	String key = httpRequest.getParameter("key");
+	
+	if (key.equals("love"))
+	{
+	    this.key = key;
+	    this.value = "you";
+	}
+        else if (key.equals("fuck"))
+	{
+	    this.key = key;
+	    this.value = "xiaojing";
+        }
+	else
+	{
+	    this.value = "not found";
+	}
+
+	httpResponse.getWriter().write(this.value);
+	
+	return ;
+    }
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String printWelcom(ModelMap model) {
-	model.addAttribute("key", "love");
-	model.addAttribute("value", "you");
+    public String showNote(ModelMap model) {	
+
+	/*	if (key.equals("love"))
+	{
+	    this.key = key;
+	    this.value = "you";
+	}
+        else if (key.equals("fuck"))
+	{
+	    this.key = key;
+	    this.value = "xiaojing";
+        }
+	
+	model.addAttribute("key", this.key);
+	model.addAttribute("value", this.value);*/
 	return "shownote";
     }
+
+
 }
--- src/main/webapp/WEB-INF/views/jsp/shownote.jsp
+++ src/main/webapp/WEB-INF/views/jsp/shownote.jsp
@@ -9,6 +9,26 @@ div#content {background-color:#EEEEEE;height:200px;width:350px;float:left;}
 h1 {margin-bottom:0;}
 h2 {margin-bottom:0;font-size:18px;}
   </style>
+
+  <script type="text/javascript">
+    function getValueFromServ(input)
+    {
+        key = input.value;
+
+        httpRe = new XMLHttpRequest();
+        httpRe.onreadystatechange=function()
+        {
+            if (httpRe.readyState == 4 &&
+                httpRe.status == 200)
+            {
+                document.getElementById("content").innerHTML =
+                  httpRe.responseText;
+            }
+        }
+        httpRe.open("GET", "search?key="+key,  false);
+        httpRe.send();
+    }
+  </script>
 </head>
 
 <body>
@@ -16,9 +36,10 @@ h2 {margin-bottom:0;font-size:18px;}
 <h1>App provided by itm94lj</h1>
 </div>
 <div id="Key">
-<h2>Key:${key}</h2>
+<h2>Key:</h2>
+<input type="text" id="Key_text" onchange="getValueFromServ(this)">
 </div>
-<div id="content">${value}</div>
+<div id="content"></div>
 </div>
 
 </body>
